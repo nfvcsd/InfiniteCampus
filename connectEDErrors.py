bee5182d3def3e78603718df777324b13e6fda08
Alex Snyder <asnyder@nfv.k12.ia.us>
2020-07-15 14:13:25 -0500

    Update connectEDErrors.py

--- connectEDErrors.py
+++ connectEDErrors.py
@@ -1,7 +1,7 @@
 #!/opt/nfv/bin/python3.8
-# from requests_oauthlib import OAuth1Session
 import csv
 import json
+import sys
 
 try:
     from requests_oauthlib import OAuth1Session
@@ -12,13 +12,22 @@ except ModuleNotFoundError:
 def get_credentials(file="config.json"):
     with open(file, "r") as config:
         data = json.load(config)
-        return (data["api_key"], data["api_secret"], data["base_url"])
+        for key in data:
+            if data[key] == "":
+                print("Please check config file")
+        else:
+            return data["api_key"], data["api_secret"], data["base_url"]
 
 
 def api_call(key, secret, url):
     test = OAuth1Session(key, client_secret=secret,)
     r = test.get(url)
     return r.json()
+
+
+def check_error(error):
+    # put code here to read a single csv line and check for error info
+    print("filler text")
 
 
 def main():
